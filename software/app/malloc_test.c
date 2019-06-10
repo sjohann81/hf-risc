a77008de0c491743fb83541900e67250d33f5546
sjohann81 <sergio.johann@acad.pucrs.br>
2019-06-10 16:39:42 -0300

    minor fixes to malloc()

--- software/app/malloc_test.c
+++ software/app/malloc_test.c
@@ -1,22 +1,23 @@
 #include <hf-risc.h>
 
+char mem_pool[8192];
+
 int main(){
 	int s = 0;
 	void *p;
-	
-	printf("heap_init(), zone size: %d bytes\n", (uint32_t)&_heap_end - (uint32_t)&_heap_start);
-	heap_init((uint32_t *)&_heap_start, (uint32_t)&_heap_end - (uint32_t)&_heap_start);
-	
+
+	heap_init((uint32_t *)&mem_pool, sizeof(mem_pool));
+
 	while (1) {
 		p = malloc(1024);
 		if (p) {
 			s += 1024;
-			printf("malloc() %d bytes ", s);
+			printf("malloc() %d bytes\n", s);
 		} else {
 			printf("malloc() failed\n");
 			break;
 		}
 	}
-	
+
 	return 0;
 }
