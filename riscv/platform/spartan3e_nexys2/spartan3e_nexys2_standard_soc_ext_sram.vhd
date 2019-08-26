9f08db6a6e7d3a8e625cf079f53e3cc10082a61d
sjohann81 <sergio.johann@acad.pucrs.br>
2019-08-26 20:35:24 -0300

    added standard SoC testbenches and templates for Nexys2 board

--- riscv/platform/spartan3e_nexys2/spartan3e_nexys2_standard_soc_ext_sram.vhd
+++ riscv/platform/spartan3e_nexys2/spartan3e_nexys2_standard_soc_ext_sram.vhd
@@ -5,45 +5,57 @@ use ieee.std_logic_unsigned.all;
 entity hfrisc_soc is
 	generic(
 		address_width: integer := 14;
-		memory_file : string := "code.txt";
-		uart_support : string := "yes"
+		memory_file : string := "code.txt"
 	);
-	port (	clk_in:		in std_logic;
-		reset_in:	in std_logic;
-		int_in:		in std_logic;
-		uart_read:	in std_logic;
-		uart_write:	out std_logic;
+	port (	clk_i:		in std_logic;
+		rst_i:		in std_logic;
 
-		sram_spi_cs:	out std_logic;
-		sram_spi_clk:	out std_logic;
-		sram_spi_mosi:	out std_logic;
-		sram_spi_miso:	in std_logic
+		-- external SRAM SPI interface
+		spi_ssn_o:	out std_logic;
+		spi_clk_o:	out std_logic;
+		spi_mosi_o:	out std_logic;
+		spi_miso_i:	in std_logic;
+
+		-- GPIO port A
+		gpioa_in:	in std_logic_vector(15 downto 0);
+		gpioa_out:	out std_logic_vector(15 downto 0);
+		gpioa_ddr:	out std_logic_vector(15 downto 0);
+		-- GPIO port B
+		gpiob_in:	in std_logic_vector(15 downto 0);
+		gpiob_out:	out std_logic_vector(15 downto 0);
+		gpiob_ddr:	out std_logic_vector(15 downto 0)
 	);
 end hfrisc_soc;
 
 architecture top_level of hfrisc_soc is
-	signal clock, boot_enable, ram_enable_n, stall, stall_cpu, busy_cpu, irq_cpu, irq_ack_cpu, exception_cpu, burst, data_b_cpu, data_h_cpu, data_access_cpu, ram_dly, rff1, reset: std_logic;
-	signal address, data_read, data_write, data_read_boot, data_read_ram, data_read_sram, irq_vector_cpu, address_cpu, data_in_cpu, data_out_cpu: std_logic_vector(31 downto 0);
+	signal clock, boot_enable_n, ram_enable_n, stall_sig, ram_dly, rff1, reset: std_logic;
+	signal address, data_read, data_write, data_read_boot, data_read_ram: std_logic_vector(31 downto 0);
 	signal ext_irq: std_logic_vector(7 downto 0);
-	signal data_we, data_w_n_ram, data_w_cpu: std_logic_vector(3 downto 0);
-	signal wr, rd, stall_dly, stall_dly2, stall_sram, hold_n: std_logic := '0';
+	signal data_we, data_w_n_ram: std_logic_vector(3 downto 0);
+
+	signal periph, periph_dly, periph_wr, periph_irq: std_logic;
+	signal data_read_periph, data_read_periph_s, data_write_periph: std_logic_vector(31 downto 0);
+
+	signal data_read_sram: std_logic_vector(31 downto 0);
+	signal data_mode: std_logic_vector(2 downto 0);
+	signal burst, wr, rd, stall_dly, stall_dly2, stall_sram: std_logic := '0';
 begin
 	-- clock divider (25MHz clock from 50MHz main clock for Spartan3 Starter Kit)
-	process (reset_in, clk_in, clock)
+	process (rst_i, clk_i, clock)
 	begin
-		if reset_in = '1' then
+		if rst_i = '1' then
 			clock <= '0';
 		else
-			if clk_in'event and clk_in='1' then
+			if clk_i'event and clk_i = '1' then
 				clock <= not clock;
 			end if;
 		end if;
 	end process;
 
 	-- reset synchronizer
-	process (clock, reset_in)
+	process (clock, rst_i)
 	begin
-		if (reset_in = '1') then
+		if (rst_i = '1') then
 			rff1 <= '1';
 			reset <= '1';
 		elsif (clock'event and clock = '1') then
@@ -53,79 +65,68 @@ begin
 	end process;
 
 
-	process (reset, clock, ext_irq, ram_enable_n)
+	process (clock, reset, stall_sram)
 	begin
 		if reset = '1' then
 			ram_dly <= '0';
+			periph_dly <= '0';
 			stall_dly <= '0';
 			stall_dly2 <= '0';
-			ext_irq <= x"00";
 		elsif clock'event and clock = '1' then
 			ram_dly <= not ram_enable_n;
+			periph_dly <= periph;
 			stall_dly <= stall_sram;
 			stall_dly2 <= stall_dly;
-			ext_irq <= "0000000" & int_in;
 		end if;
 	end process;
 
-	stall <= '0';
-	boot_enable <= '1' when address(31 downto 28) = "0000" else '0';
-	ram_enable_n <= '0' when address(31 downto 28) = "0100" else '1';
+	boot_enable_n <= '0' when (address(31 downto 28) = "0000" and stall_sig = '0') else '1';
+	ram_enable_n <= '0' when (address(31 downto 28) = "0100" and stall_sig = '0') else '1';
 	rd <= '1' when (address(31 downto 28) = "0110" and data_we = "0000" and stall_dly2 = '0') else '0';
 	wr <= '1' when (address(31 downto 28) = "0110" and data_we /= "0000" and stall_dly2 = '0') else '0';
-	data_read <= data_read_boot when address(31 downto 28) = "0000" and ram_dly = '0' else
-	data_read_sram when address(31 downto 28) = "0110" or stall_dly2 = '1' else data_read_ram;
+	data_read <= data_read_periph when periph = '1' or periph_dly = '1' else data_read_boot when address(31 downto 28) = "0000" and ram_dly = '0' else
+			data_read_sram when address(31 downto 28) = "0110" or stall_dly2 = '1' else data_read_ram;
 	data_w_n_ram <= not data_we;
 	burst <= '0';
+	stall_sig <= stall_sram;
 
-	-- HF-RISC core
-	core: entity work.datapath
-	port map(	clock => clock,
-			reset => reset,
-			stall => stall_cpu,
-			irq_vector => irq_vector_cpu,
-			irq => irq_cpu,
-			irq_ack => irq_ack_cpu,
-			exception => exception_cpu,
-			address => address_cpu,
-			data_in => data_in_cpu,
-			data_out => data_out_cpu,
-			data_w => data_w_cpu,
-			data_b => data_b_cpu,
-			data_h => data_h_cpu,
-			data_access => data_access_cpu
+	ext_irq <= "0000000" & periph_irq;
+
+	-- HF-RISCV core
+	processor: entity work.processor
+	port map(	clk_i => clock,
+			rst_i => reset,
+			stall_i => stall_sig,
+			addr_o => address,
+			data_i => data_read,
+			data_o => data_write,
+			data_w_o => data_we,
+			data_mode_o => data_mode,
+			extio_in => ext_irq,
+			extio_out => open
 	);
 
-	-- peripherals / busmux logic
-	peripherals_busmux: entity work.busmux
-	generic map(
-		uart_support => uart_support
-	)
+	data_read_periph <= data_read_periph_s(7 downto 0) & data_read_periph_s(15 downto 8) & data_read_periph_s(23 downto 16) & data_read_periph_s(31 downto 24);
+	data_write_periph <= data_write(7 downto 0) & data_write(15 downto 8) & data_write(23 downto 16) & data_write(31 downto 24);
+	periph_wr <= '1' when data_we /= "0000" else '0';
+	periph <= '1' when address(31 downto 28) = x"e" else '0';
+
+	peripherals: entity work.peripherals
 	port map(
-		clock => clock,
-		reset => reset,
-
-		stall => stall,
-
-		stall_cpu => stall_cpu,
-		irq_vector_cpu => irq_vector_cpu,
-		irq_cpu => irq_cpu,
-		irq_ack_cpu => irq_ack_cpu,
-		exception_cpu => exception_cpu,
-		address_cpu => address_cpu,
-		data_in_cpu => data_in_cpu,
-		data_out_cpu => data_out_cpu,
-		data_w_cpu => data_w_cpu,
-		data_access_cpu => data_access_cpu,
-
-		addr_mem => address,
-		data_read_mem => data_read,
-		data_write_mem => data_write,
-		data_we_mem => data_we,
-		extio_in => ext_irq,
-		extio_out => open,
-		uart_read => uart_read,
-		uart_write => uart_write
+		clk_i => clock,
+		rst_i => reset,
+		addr_i => address,
+		data_i => data_write_periph,
+		data_o => data_read_periph_s,
+		sel_i => periph,
+		wr_i => periph_wr,
+		irq_o => periph_irq,
+		gpioa_in => gpioa_in,
+		gpioa_out => gpioa_out,
+		gpioa_ddr => gpioa_ddr,
+		gpiob_in => gpiob_in,
+		gpiob_out => gpiob_out,
+		gpiob_ddr => gpiob_ddr
 	);
 
 	sram_ctrl_core: entity work.spi_sram_ctrl
@@ -135,16 +136,16 @@ begin
 			data_i => data_write,
 			data_o => data_read_sram,
 			burst_i => burst,
-			bmode_i => data_b_cpu,
-			hmode_i => data_h_cpu,
+			bmode_i => data_mode(2),
+			hmode_i => data_mode(1),
 			wr_i => wr,
 			rd_i => rd,
 			data_ack_o => open,
 			cpu_stall_o => stall_sram,
-			spi_cs_n_o => sram_spi_cs,
-			spi_clk_o => sram_spi_clk,
-			spi_mosi_o => sram_spi_mosi,
-			spi_miso_i => sram_spi_miso
+			spi_cs_n_o => spi_ssn_o,
+			spi_clk_o => spi_clk_o,
+			spi_mosi_o => spi_mosi_o,
+			spi_miso_i => spi_miso_i
 	);
 
 	-- instruction and data memory (boot RAM)
@@ -152,7 +153,7 @@ begin
 	generic map (memory_type => "DEFAULT")
 	port map (
 		clk			=> clock,
-		enable			=> boot_enable,
+		enable			=> boot_enable_n,
 		write_byte_enable	=> "0000",
 		address			=> address(31 downto 2),
 		data_write		=> (others => '0'),
