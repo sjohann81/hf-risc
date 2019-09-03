e789e7d22f26b79cbc4a9e27e34ee35a2614edef
sjohann81 <sergio.johann@acad.pucrs.br>
2019-09-03 17:05:21 -0300

    added ASSoC HAL support

---
+++ software/app/assoc_test/assoc.h
@@ -0,0 +1,10 @@
+#define RESERVED_BASE			0xe0ff0000
+#define SYNC_MAC_OPER			(*(volatile uint32_t *)(RESERVED_BASE + 0x4000))
+#define SYNC_MAC_RESULT			(*(volatile uint32_t *)(RESERVED_BASE + 0x4010))
+#define ASYNC_MAC_OPER			(*(volatile uint32_t *)(RESERVED_BASE + 0x4400))
+#define ASYNC_MAC_RESULT		(*(volatile uint32_t *)(RESERVED_BASE + 0x4410))
+#define DE_PAUSE			(*(volatile uint32_t *)(RESERVED_BASE + 0x4800))
+#define DE_CONFIG			(*(volatile uint32_t *)(RESERVED_BASE + 0x4810))
+#define DE_CDE_SEL			(*(volatile uint32_t *)(RESERVED_BASE + 0x4820))
+#define DE_MDE_SEL			(*(volatile uint32_t *)(RESERVED_BASE + 0x4830))
+#define DE_CDE_CTRL			(*(volatile uint32_t *)(RESERVED_BASE + 0x4840))
---
+++ software/app/assoc_test/assoc_test.c
@@ -0,0 +1,18 @@
+#include <hf-risc.h>
+#include "assoc.h"
+
+int main(void){
+	SYNC_MAC_OPER = 0xaabbccdd;
+	printf("SYNC_MAC_RESULT: %08x\n", SYNC_MAC_RESULT);
+	SYNC_MAC_OPER ^= -1;
+	ASYNC_MAC_OPER = 0xddbbccaa;
+	printf("ASYNC_MAC_RESULT: %08x\n", ASYNC_MAC_RESULT);
+	ASYNC_MAC_OPER ^= -1;
+	DE_PAUSE ^= -1;
+	DE_CONFIG ^= -1;
+	DE_CDE_SEL ^= -1;
+	DE_MDE_SEL ^= -1;
+	DE_CDE_CTRL ^= -1;
+
+	return 0;
+}
