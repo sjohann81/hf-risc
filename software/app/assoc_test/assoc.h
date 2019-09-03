d97d9b94fa967ec2241de5d7c5de43dca58a0bc2
sjohann81 <sergio.johann@acad.pucrs.br>
2019-09-03 17:18:41 -0300

    updated ASSoC HAL

--- software/app/assoc_test/assoc.h
+++ software/app/assoc_test/assoc.h
@@ -3,6 +3,7 @@
 #define SYNC_MAC_RESULT			(*(volatile uint32_t *)(RESERVED_BASE + 0x4010))
 #define ASYNC_MAC_OPER			(*(volatile uint32_t *)(RESERVED_BASE + 0x4400))
 #define ASYNC_MAC_RESULT		(*(volatile uint32_t *)(RESERVED_BASE + 0x4410))
+#define ASYNC_MAC_RST			(*(volatile uint32_t *)(RESERVED_BASE + 0x4420))
 #define DE_PAUSE			(*(volatile uint32_t *)(RESERVED_BASE + 0x4800))
 #define DE_CONFIG			(*(volatile uint32_t *)(RESERVED_BASE + 0x4810))
 #define DE_CDE_SEL			(*(volatile uint32_t *)(RESERVED_BASE + 0x4820))
--- software/app/assoc_test/assoc_test.c
+++ software/app/assoc_test/assoc_test.c
@@ -8,6 +8,7 @@ int main(void){
 	ASYNC_MAC_OPER = 0xddbbccaa;
 	printf("ASYNC_MAC_RESULT: %08x\n", ASYNC_MAC_RESULT);
 	ASYNC_MAC_OPER ^= -1;
+	ASYNC_MAC_RST ^= -1;
 	DE_PAUSE ^= -1;
 	DE_CONFIG ^= -1;
 	DE_CDE_SEL ^= -1;
