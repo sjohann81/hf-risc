3f9b1a7a45b87cf0202de6d3297d927b2faf7475
sergio johann filho <sergio.johann@acad.pucrs.br>
2020-04-29 16:40:49 -0300

    added new examples

---
+++ software/app/interrupt_test3.c
@@ -0,0 +1,37 @@
+#include <hf-risc.h>
+
+void timer1ctc_handler(void)
+{
+	printf("TIMER1CTC %d\n", TIMER0);
+}
+
+void timer1ocr_handler(void)
+{
+	printf("TIMER1OCR %d\n", TIMER0);
+	
+	/* schedule next interrupt (half period) */
+	TIMER1OCR = (TIMER1OCR + 196) % 19531;
+}
+
+
+int main(void){
+	TIMER1PRE = TIMERPRE_DIV256;
+
+	/* TIMER1 base frequency: 5M cycles */
+	/* 5000000 / 256 = 19531.25 */
+	TIMER1CTC = 19531;
+	
+	/* TIMER1 alt frequency: 100k cycles */
+	/* 100000 / 256 = 390.625 */
+	TIMER1OCR = 391;
+
+	/* unlock TIMER1 for reset */
+	TIMER1 = TIMERSET;
+	TIMER1 = 0;
+
+	/* enable interrupt mask for TIMER1 CTC and OCR events */
+	TIMERMASK |= MASK_TIMER1CTC | MASK_TIMER1OCR;
+
+	for(;;){
+	}
+}
---
+++ software/app/tasker.c
@@ -0,0 +1,90 @@
+#include <hf-risc.h>
+
+#define N_TASKS		4
+
+typedef uint32_t jmp_buf[20];
+int32_t setjmp(jmp_buf env);
+void longjmp(jmp_buf env, int32_t val);
+
+static jmp_buf jmp[N_TASKS];
+static int cur;
+
+void context_switch()
+{
+	if (!setjmp(jmp[cur])) {
+		if (N_TASKS == ++cur)
+			cur = 0;
+		longjmp(jmp[cur], 1);
+	}
+}
+
+void idle_task()
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+        cur = N_TASKS - 1;		/* the first thread to context switch is this one */
+	
+	setjmp(jmp[N_TASKS - 1]);
+
+	while (1) {			/* thread body */
+		context_switch();
+		
+		printf("idle task...\n");
+		delay_ms(100);
+	}
+}
+
+void task2(void)
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+
+	if (!setjmp(jmp[2]))
+		idle_task();
+
+	while (1) {			/* thread body */
+		context_switch();
+		
+		printf("task 2...\n");
+		delay_ms(100);
+	}
+}
+
+void task1(void)
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+
+	if (!setjmp(jmp[1]))
+		task2();
+
+	while (1) {			/* thread body */
+		context_switch();
+		
+		printf("task 1...\n");
+		delay_ms(100);
+	}
+}
+
+void task0(void)
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+
+	if (!setjmp(jmp[0]))
+		task1();
+
+	while (1) {			/* thread body */
+		context_switch();
+		
+		printf("task 0...\n");
+		delay_ms(100);
+	}
+}
+
+int main(void)
+{
+	task0();
+    
+	return 0;
+}
