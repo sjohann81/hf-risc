3d87891276518ea7694fbdaf451d6498fe118637
Guilherme Silva <pereiraguilherme997@gmail.com>
2020-05-16 12:55:12 -0300

    Creating new task struct and functions

--- software/app/tp1.c
+++ software/app/tp1.c
@@ -5,24 +5,78 @@
 #include <hf-risc.h>
 #include <list.h>
 
-#define N_TASKS 2 
+#define N_TASKS 4  
+#define READY   0
+#define RUNNING 1
+#define BLOCKED 2
 
 char mem_pool[8192];
 
 typedef uint32_t jmp_buf[20];
 int32_t setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int32_t val);
-
+/***** Global Variables *****/
 static jmp_buf jmp[N_TASKS];
 static int cur;
+struct list *queue;
+int interr = 0;
 
-void context_switch()
+/***** Struct definition****/
+struct task {
+    int id;
+    int computTime;
+    int period;
+    int deadline;
+    int computTimeCount;
+    int status;
+};
+
+void context_switch(int taskId)
 {
 	if (!setjmp(jmp[cur])) {
-		if (N_TASKS == ++cur)
-			cur = 0;
+        cur = taskId;
 		longjmp(jmp[cur], 1);
 	}
+}
+
+struct task *getNextTask(){
+    struct task *cTask = list_get(queue, 0);
+    int i, auxIndex = 0;
+    int menorP = cTask->period;
+
+    for(i=1; i < (N_TASKS-1); i++){
+        cTask = list_get(queue, i);
+        if(cTask->period < menorP){
+            menorP = cTask->period;
+            auxIndex = i;
+        }
+    }
+    struct task *returnTask = list_get(queue, auxIndex);
+    return returnTask;
+}
+
+struct task *getRunningTask(){
+    struct task *rTask;
+    int i;
+    for(i=0; i< (N_TASKS-1); i++){
+        rTask = list_get(queue, i);
+        if(rTask->status == RUNNING) return rTask;
+    }
+    return NULL;
+}
+
+// Como inicializar o running
+void scheduler(){
+    struct task *runningT = getRunningTask();
+    if(runningT == NULL){
+        struct task *curTask = getNextTask();
+        context_switch(curTask->id);
+    } else if(interr == 1){
+        if(runningT->computTimeCount != runningT->computTime){
+            //task não acabou de executar ainda e tem que ser preemptada
+            //status passa pra BLOCKED
+        }
+    } 
 }
 
 void idle_task()
@@ -34,15 +88,13 @@ void idle_task()
 	setjmp(jmp[N_TASKS - 1]);
 
 	while (1) {			/* thread body */
-		context_switch();
-		
+		scheduler();
 		printf("idle task...\n");
 		delay_ms(100);
 	}
 }
 
-//task sample
-void task(void)
+void task2_func()
 {
 	volatile char cushion[1000];	/* reserve some stack space */
 	cushion[0] = '@';		/* don't optimize my cushion away */
@@ -51,8 +103,37 @@ void task(void)
 		idle_task();
 
 	while (1) {			/* thread body */
-		context_switch();
-		
+		scheduler();
+		printf("task 2...\n");
+		delay_ms(100);
+	}
+}
+
+void task1_func()
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+
+	if (!setjmp(jmp[1]))
+		task2();
+
+	while (1) {			/* thread body */
+		scheduler();
+		printf("task 2...\n");
+		delay_ms(100);
+	}
+}
+
+void task0_func()
+{
+	volatile char cushion[1000];	/* reserve some stack space */
+	cushion[0] = '@';		/* don't optimize my cushion away */
+
+	if (!setjmp(jmp[0]))
+		task1();
+
+	while (1) {			/* thread body */
+		scheduler();
 		printf("task 2...\n");
 		delay_ms(100);
 	}
@@ -61,11 +142,9 @@ void task(void)
 int main(void)
 {   
     int32_t i =0;
-    struct list *queue;
 
     /* intialize heap because of list use*/
     heap_init((uint32_t *)&mem_pool, sizeof(mem_pool));
-
 
     queue = list_init();
     /* reserve space for the incoming tasks */
@@ -75,7 +154,33 @@ int main(void)
     }
     printf("Number of nodes %d\n", list_count(queue));
 	
-    //task();
-    
+    struct task task0, task1, task2;
+    task0.id = 0;
+    task0.computTime  = 1;
+    task0.period = 8;
+    task0.deadline = 4;
+    task0.computTimeCount = 0;
+    task0.status = READY;
+
+    task1.id = 1;
+    task1.computTime  = 2;
+    task1.period = 10;
+    task1.deadline = 5;
+    task1.computTimeCount = 0;
+    task1.status = READY;
+
+    task2.id = 2;
+    task2.computTime  = 2;
+    task2.period = 15;
+    task2.deadline = 10;
+    task2.computTimeCount = 0;
+    task2.status = READY;
+
+    list_append(queue, &task0);
+    list_append(queue, &task1);
+    list_append(queue, &task2);
+
+    task0_func();
+
 	return 0;
 }
