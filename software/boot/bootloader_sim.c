f25e86e22e639dbc1374d0db95a2ccae2aca877c
sergio johann filho <sergio.johann@acad.pucrs.br>
2022-01-09 11:57:52 -0300

    fix VHDL sim bootloader

--- software/boot/bootloader_sim.c
+++ software/boot/bootloader_sim.c
@@ -6,15 +6,15 @@ int32_t main(void){
 
 	printf("boot\n");
 
-	uint32_t *ptr, *ptr2;
+//	uint32_t *ptr, *ptr2;
 
-	ptr = (uint32_t *)RAM_BASE;
-	ptr2 = (uint32_t *)EXT_SRAM;
-	memcpy(ptr2, ptr, 4096);
+//	ptr = (uint32_t *)RAM_BASE;
+//	ptr2 = (uint32_t *)EXT_SRAM;
+//	memcpy(ptr2, ptr, 4096);
 
-	funcPtr = (void (*)(void *))(EXT_SRAM);
+//	funcPtr = (void (*)(void *))(EXT_SRAM);
 
-//	funcPtr = (void (*)(void *))(RAM_BASE);
+	funcPtr = (void (*)(void *))(RAM_BASE);
 	funcPtr();
 
 	return 0;
