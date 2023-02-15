bd1bb03c95c1d6e79c0be7ac8f980712a44f8119
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-02-15 14:42:24 -0300

    add GPIO example

---
+++ software/app/gpio.c
@@ -0,0 +1,33 @@
+#include <hf-risc.h>
+
+void porta_handler(void)
+{
+	printf("PAIN: %04x\n", PAIN);
+}
+
+void portb_handler(void)
+{
+	printf("PBIN: %04x\n", PBIN);
+}
+
+int main(void){
+	/* enable interrupt mask for PORTA and PORTB inputs */
+	GPIOMASK |= MASK_PAIN | MASK_PBIN;
+
+	/* enable PORTA input change mask for pins 5 and 4 */
+	PAINMASK |= MASK_P5 | MASK_P4;
+
+	/* enable PORTB input change mask for pins 11, 10 and 1 */
+	PBINMASK |= MASK_P11 | MASK_P10 | MASK_P1;
+	
+	/* set PORTA pin 0 as an output */
+	PADDR |= MASK_P0;
+
+	for(;;){
+		/* toggle pin 0 */
+		PAOUT ^= MASK_P0;
+		
+		printf(".");
+		delay_ms(1000);
+	}
+}
