43198273bfa32b9f8924e89bd68425a8662d6a36
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-02-15 14:44:35 -0300

    add basic SoC / AXI stream simulation testbenchf

---
+++ riscv/sim/hf-riscv_basic_soc_axis_tb.vhd
@@ -0,0 +1,366 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_textio.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use ieee.numeric_std.all;
+
+entity tb is
+	generic(
+		address_width: integer := 15;
+		memory_file : string := "code.txt";
+		log_file: string := "out.txt";
+		uart_support : string := "no"
+	);
+end tb;
+
+architecture tb of tb is
+	signal clock_in, reset, data, stall, stall_sig: std_logic := '0';
+	signal uart_read, uart_write: std_logic;
+	signal boot_enable_n, ram_enable_n, ram_dly: std_logic;
+	signal address, data_read, data_write, data_read_boot, data_read_ram: std_logic_vector(31 downto 0);
+	signal ext_irq: std_logic_vector(7 downto 0);
+	signal data_we, data_w_n_ram: std_logic_vector(3 downto 0);
+
+	signal periph, periph_dly, periph_wr, periph_irq: std_logic;
+	signal data_read_periph, data_read_periph_s, data_write_periph: std_logic_vector(31 downto 0);
+	signal gpioa_in, gpioa_out, gpioa_ddr: std_logic_vector(15 downto 0);
+	signal gpiob_in, gpiob_out, gpiob_ddr: std_logic_vector(15 downto 0);
+	signal gpio_sig, gpio_sig2, gpio_sig3: std_logic := '0';
+	
+	signal ext_periph, ext_periph_dly: std_logic;
+	signal reset_n, rxd, txd: std_logic;
+	signal data_read_axis, data_read_axis_s: std_logic_vector(31 downto 0);
+	signal m_axis_tready_s, m_axis_tvalid_s, s_axis_tready_s, s_axis_tvalid_s : std_logic;
+	signal m_axis_tdata_s, s_axis_tdata_s : std_logic_vector(7 downto 0);
+	signal uart_divisor_s: std_logic_vector(11 downto 0);
+begin
+
+	process						--25Mhz system clock
+	begin
+		clock_in <= not clock_in;
+		wait for 20 ns;
+		clock_in <= not clock_in;
+		wait for 20 ns;
+	end process;
+
+	process
+	begin
+		wait for 4 ms;
+		gpio_sig <= not gpio_sig;
+		gpio_sig2 <= not gpio_sig2;
+		wait for 100 us;
+		gpio_sig <= not gpio_sig;
+		gpio_sig2 <= not gpio_sig2;
+	end process;
+
+	process
+	begin
+		wait for 5 ms;
+		gpio_sig3 <= not gpio_sig3;
+		wait for 5 ms;
+		gpio_sig3 <= not gpio_sig3;
+	end process;
+
+	gpioa_in <= x"00" & "0000" & gpio_sig & "000";
+	gpiob_in <= "10000" & gpio_sig3 & "00" & "00000" & gpio_sig2 & "00";
+
+	process
+	begin
+		stall <= not stall;
+		wait for 123 ns;
+		stall <= not stall;
+		wait for 123 ns;
+	end process;
+	
+	process				-- simulate a byte being received on the AXI UART @ 115200bps
+	begin
+		rxd <= '1';
+		wait for 10000 ns;
+		rxd <= '0';		-- start bit
+		wait for 8680 ns;
+		rxd <= '1';		-- 55
+		wait for 8680 ns;
+		rxd <= '0';
+		wait for 8680 ns;
+		rxd <= '1';
+		wait for 8680 ns;
+		rxd <= '0';
+		wait for 8680 ns;
+		rxd <= '1';
+		wait for 8680 ns;
+		rxd <= '0';
+		wait for 8680 ns;
+		rxd <= '1';
+		wait for 8680 ns;
+		rxd <= '0';
+		wait for 8680 ns;
+		rxd <= '1';		-- stop bit
+		wait;
+	end process;
+
+	reset <= '0', '1' after 5 ns, '0' after 500 ns;
+	stall_sig <= '0'; --stall;
+	ext_irq <= "0000000" & periph_irq;
+
+
+	boot_enable_n <= '0' when (address(31 downto 28) = "0000" and stall_sig = '0') or reset = '1' else '1';
+	ram_enable_n <= '0' when (address(31 downto 28) = "0100" and stall_sig = '0') or reset = '1' else '1';
+	data_read <= data_read_axis when ext_periph = '1' or ext_periph_dly = '1' else data_read_periph when periph = '1' or periph_dly = '1' else data_read_boot when address(31 downto 28) = "0000" and ram_dly = '0' else data_read_ram;
+	data_w_n_ram <= not data_we;
+
+	process(clock_in, reset)
+	begin
+		if reset = '1' then
+			ram_dly <= '0';
+			periph_dly <= '0';
+			ext_periph_dly <= '0';
+		elsif clock_in'event and clock_in = '1' then
+			ram_dly <= not ram_enable_n;
+			periph_dly <= periph;
+			ext_periph_dly <= ext_periph;
+		end if;
+	end process;
+
+	-- HF-RISCV core
+	processor: entity work.processor
+	port map(	clk_i => clock_in,
+			rst_i => reset,
+			stall_i => stall_sig,
+			addr_o => address,
+			data_i => data_read,
+			data_o => data_write,
+			data_w_o => data_we,
+			data_mode_o => open,
+			extio_in => ext_irq,
+			extio_out => open
+	);
+
+	data_read_periph <= data_read_periph_s(7 downto 0) & data_read_periph_s(15 downto 8) & data_read_periph_s(23 downto 16) & data_read_periph_s(31 downto 24);
+	data_write_periph <= data_write(7 downto 0) & data_write(15 downto 8) & data_write(23 downto 16) & data_write(31 downto 24);
+	periph_wr <= '1' when data_we /= "0000" else '0';
+	periph <= '1' when address(31 downto 24) = x"e1" else '0';
+
+	peripherals: entity work.peripherals
+	port map(
+		clk_i => clock_in,
+		rst_i => reset,
+		addr_i => address,
+		data_i => data_write_periph,
+		data_o => data_read_periph_s,
+		sel_i => periph,
+		wr_i => periph_wr,
+		irq_o => periph_irq,
+		gpioa_in => gpioa_in,
+		gpioa_out => gpioa_out,
+		gpioa_ddr => gpioa_ddr,
+		gpiob_in => gpiob_in,
+		gpiob_out => gpiob_out,
+		gpiob_ddr => gpiob_ddr
+	);
+	
+	
+	data_read_axis <= data_read_axis_s(7 downto 0) & data_read_axis_s(15 downto 8) & data_read_axis_s(23 downto 16) & data_read_axis_s(31 downto 24);
+	reset_n <= not reset;
+	
+	-- AXI stream / CPU interface core for UART
+	if_axis_core1: entity work.if_axis
+	port map(
+		-- external CPU interface signals
+		addr_i => address,
+		data_i => data_write_periph,
+		data_o => data_read_axis_s,
+		data_access_o => ext_periph,
+		data_w_i => periph_wr,
+		-- external core signals
+		uart_divisor_o => uart_divisor_s,
+		-- AXI stream interface signals
+		axis_aclk_i => clock_in,
+		axis_aresetn_i => reset_n,
+		-- master axi stream interface (connected to UART slave interface)
+		m_axis_tready_i => s_axis_tready_s,
+		m_axis_tvalid_o => s_axis_tvalid_s,
+		m_axis_tdata_o => s_axis_tdata_s,
+		-- slave axi stream interface (connected to UART master interface)
+		s_axis_tready_o => m_axis_tready_s,
+		s_axis_tvalid_i => m_axis_tvalid_s,
+		s_axis_tdata_i => m_axis_tdata_s
+	);
+
+	-- AXI stream UART core
+	uart_axis_core: entity work.uart_axis
+	port map(
+		-- external core signals
+		divisor_i => uart_divisor_s,
+		rxd_i => rxd,
+		txd_o => txd,
+		-- AXI stream interface signals
+		axis_aclk_i => clock_in,
+		axis_aresetn_i => reset_n,
+		-- master axi stream interface
+		m_axis_tready_i => m_axis_tready_s,
+		m_axis_tvalid_o => m_axis_tvalid_s,
+		m_axis_tdata_o => m_axis_tdata_s,
+		-- slave axi stream interface
+		s_axis_tready_o => s_axis_tready_s,
+		s_axis_tvalid_i => s_axis_tvalid_s,
+		s_axis_tdata_i => s_axis_tdata_s
+	);
+		
+
+	-- boot ROM
+	boot0lb: entity work.boot_ram
+	generic map (	memory_file => "boot.txt",
+					data_width => 8,
+					address_width => 12,
+					bank => 0)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(11 downto 2),
+		cs_n 	=> boot_enable_n,
+		we_n	=> '1',
+		data_i	=> (others => '0'),
+		data_o	=> data_read_boot(7 downto 0)
+	);
+
+	boot0ub: entity work.boot_ram
+	generic map (	memory_file => "boot.txt",
+					data_width => 8,
+					address_width => 12,
+					bank => 1)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(11 downto 2),
+		cs_n 	=> boot_enable_n,
+		we_n	=> '1',
+		data_i	=> (others => '0'),
+		data_o	=> data_read_boot(15 downto 8)
+	);
+
+	boot1lb: entity work.boot_ram
+	generic map (	memory_file => "boot.txt",
+					data_width => 8,
+					address_width => 12,
+					bank => 2)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(11 downto 2),
+		cs_n 	=> boot_enable_n,
+		we_n	=> '1',
+		data_i	=> (others => '0'),
+		data_o	=> data_read_boot(23 downto 16)
+	);
+
+	boot1ub: entity work.boot_ram
+	generic map (	memory_file => "boot.txt",
+					data_width => 8,
+					address_width => 12,
+					bank => 3)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(11 downto 2),
+		cs_n 	=> boot_enable_n,
+		we_n	=> '1',
+		data_i	=> (others => '0'),
+		data_o	=> data_read_boot(31 downto 24)
+	);
+
+	-- RAM
+	memory0lb: entity work.bram
+	generic map (	memory_file => memory_file,
+					data_width => 8,
+					address_width => address_width,
+					bank => 0)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(address_width -1 downto 2),
+		cs_n 	=> ram_enable_n,
+		we_n	=> data_w_n_ram(0),
+		data_i	=> data_write(7 downto 0),
+		data_o	=> data_read_ram(7 downto 0)
+	);
+
+	memory0ub: entity work.bram
+	generic map (	memory_file => memory_file,
+					data_width => 8,
+					address_width => address_width,
+					bank => 1)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(address_width -1 downto 2),
+		cs_n 	=> ram_enable_n,
+		we_n	=> data_w_n_ram(1),
+		data_i	=> data_write(15 downto 8),
+		data_o	=> data_read_ram(15 downto 8)
+	);
+
+	memory1lb: entity work.bram
+	generic map (	memory_file => memory_file,
+					data_width => 8,
+					address_width => address_width,
+					bank => 2)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(address_width -1 downto 2),
+		cs_n 	=> ram_enable_n,
+		we_n	=> data_w_n_ram(2),
+		data_i	=> data_write(23 downto 16),
+		data_o	=> data_read_ram(23 downto 16)
+	);
+
+	memory1ub: entity work.bram
+	generic map (	memory_file => memory_file,
+					data_width => 8,
+					address_width => address_width,
+					bank => 3)
+	port map(
+		clk 	=> clock_in,
+		addr 	=> address(address_width -1 downto 2),
+		cs_n 	=> ram_enable_n,
+		we_n	=> data_w_n_ram(3),
+		data_i	=> data_write(31 downto 24),
+		data_o	=> data_read_ram(31 downto 24)
+	);
+
+	-- debug process
+	debug:
+	if uart_support = "no" generate
+		process(clock_in, address)
+			file store_file : text open write_mode is "debug.txt";
+			variable hex_file_line : line;
+			variable c : character;
+			variable index : natural;
+			variable line_length : natural := 0;
+		begin
+			if clock_in'event and clock_in = '1' then
+				if address = x"f00000d0" and data = '0' then
+					data <= '1';
+					index := conv_integer(data_write(30 downto 24));
+					if index /= 10 then
+						c := character'val(index);
+						write(hex_file_line, c);
+						line_length := line_length + 1;
+					end if;
+					if index = 10 or line_length >= 72 then
+						writeline(store_file, hex_file_line);
+						line_length := 0;
+					end if;
+				else
+					data <= '0';
+				end if;
+			end if;
+		end process;
+	end generate;
+
+	process(clock_in, reset, address)
+	begin
+		if reset = '1' then
+		elsif clock_in'event and clock_in = '0' then
+			assert address /= x"e0000000" report "end of simulation" severity failure;
+			assert (address < x"50000000") or (address >= x"e0000000") report "out of memory region" severity failure;
+			assert address /= x"40000104" report "handling IRQ" severity warning;
+		end if;
+	end process;
+
+end tb;
+
