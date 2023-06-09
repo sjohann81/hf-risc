3e259753c204c2195a7f58ec8c75df234e266d70
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-06-08 21:30:03 -0300

    timer overflow irq

--- software/app/interrupt_test.c
+++ software/app/interrupt_test.c
@@ -30,11 +30,16 @@ void timer1ocr_handler(void)
 	printf("TIMER1OCR\n");
 }
 
+void timer1ovf_handler(void)
+{
+	printf("TIMER1OVF\n");
+}
+
 int main(void){
 	int32_t val;
 	
 	//TIMER1PRE = TIMERPRE_DIV64;
-	TIM1->PRE = TIMERPRE_DIV64;
+	TIM1->PRE = TIMERPRE_DIV16;
 
 	/* unlock TIMER1 for reset */
 	//TIMER1 = TIMERSET;
@@ -44,13 +49,13 @@ int main(void){
 
 	/* TIMER1 PWM duty cycle, 80% */
 	//TIMER1OCR = 8000;
-	TIM1->OCR = 8000;
+	TIM1->OCR = 43750;
 	//TIMER1CTC = 10000;
-	TIM1->CTC = 10000;
+	TIM1->CTC = 50000;
 
 	/* enable interrupt masks for TIMER0 and TIMER1 CTC and OCR events */
 	//TIMERMASK |= (MASK_TIMER0A | MASK_TIMER0B | MASK_TIMER1CTC | MASK_TIMER1OCR);
-	TIMER->MASK |= (MASK_TIMER0A | MASK_TIMER0B | MASK_TIMER1CTC | MASK_TIMER1OCR);
+	TIMER->MASK |= (MASK_TIMER0A | MASK_TIMER0B | MASK_TIMER1CTC | MASK_TIMER1OCR | MASK_TIMER1OVF);
 	
 	/* enable interrupt mask for PORTA and PORTB inputs */
 	//GPIOMASK |= MASK_PAIN | MASK_PBIN;
