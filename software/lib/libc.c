1cf68fa1fb069348f828725cd60bce705ef48e02
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2024-11-08 16:27:53 -0300

    add map function to libc

--- software/lib/libc.c
+++ software/lib/libc.c
@@ -54,6 +54,10 @@ void delay_us(uint32_t usec)
 	}
 }
 
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 /*
 minimal custom C library
 */
